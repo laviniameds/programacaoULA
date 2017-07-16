a721e19538596580b0d87bf97c7f7cf346fb8ded
Lavinia Medeiros <laviniameds@icloud.com>
2017-07-16 19:16:53 -0300

    programa finalizado

--- Program.cs
+++ Program.cs
@@ -4,42 +4,23 @@ namespace C_
 {
     class Program
     {
+        static string toBinary(string valor){
+            return Convert.ToString(Convert.ToInt32(valor), 2).PadLeft(5, '0');
+        }
         static void Main(string[] args)
-        {
-            
-            int seletor = 0;
-            Console.WriteLine("Digite a operação: \n1 - AND \n2 - OR\n3 - XOR\n4 - NAND\n5 - SOMA INT\n6 - SOMA NAT\n7 - SUBTRAIR\n8 - DESL ESQ\n9 - DESL DIR\n10 - DESL DIR A\n11 - ROLL LEFT\n12 - COMPARADOR");
-            string op = Console.ReadLine();
-            switch(op){
-                default: Console.WriteLine("operação inválida");
-                break;
-                case "1": seletor = 00000;
-                break;
-                case "2": seletor = 00001;
-                break;
-                case "3": seletor = 00010;
-                break;
-                case "4": seletor = 00011;
-                break;
-                case "5": seletor = 00100;
-                break;
-                case "6": seletor = 00101;
-                break;
-                case "7": seletor = 00110;
-                break;
-                case "8": seletor = 00111;
-                break;
-                case "9": seletor = 01000;
-                break;
-                case "10": seletor = 01001;
-                break;
-                case "11": seletor = 01010;
-                break;
-                case "12": seletor = 01011;
-                break;
-            }
-            Console.WriteLine("");
-            
+        {        
+            Console.WriteLine("Digite a operação: \n0 - AND \n1 - OR\n2 - XOR\n3 - NAND\n4 - SOMA INT\n5 - SOMA NAT\n6 - SUBTRAIR\n7 - DESL ESQ\n8 - DESL DIR\n9 - DESL DIR A\n10 - ROLL LEFT\n11 - COMPARADOR");
+            string op = toBinary(Console.ReadLine());
+            Console.WriteLine("Digite o numero correspondente ao regDEST Ex: 1, 2 .. 32");
+            string regDEST = toBinary(Console.ReadLine());
+            Console.WriteLine("Digite o numero correspondente ao RS Ex: 1, 2 .. 32");
+            string RS = toBinary(Console.ReadLine());
+            Console.WriteLine("Digite o numero correspondente ao RT Ex: 1, 2 .. 32");
+            string RT = toBinary(Console.ReadLine());
+            string constante = "000000000000";
+            string resultBin = op + regDEST + RS + RT + constante;
+            Console.WriteLine("O resultado é: {0}", resultBin);
+            Console.WriteLine(resultBin.Length);
         }
     }
 }
